a9ee671f7dbd80749f007fbc3febecc3cba79328
Darshan <darshangohad28525@gmail.com>
2024-05-03 13:27:12 +0530

    second commit

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonService } from 'src/app/common.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  register: any = FormGroup;
+  id: any = 1;
+  constructor(private fb: FormBuilder, private router: Router, private commServ:CommonService) {}
+
+  ngOnInit(): void {
+    this.register = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+    });
+  }
+
+  registerSubmit(data: any) {
+    let dataToPass = {
+      name: data.name, email:data.email ,
+      id:this.id++ 
+    }
+
+    this.commServ.addUser(dataToPass).subscribe((data:any)=>{
+      alert("User registered")
+    })
+  }
+
+  gotoLogin() {
+    this.router.navigate(['']);
+  }
+}
